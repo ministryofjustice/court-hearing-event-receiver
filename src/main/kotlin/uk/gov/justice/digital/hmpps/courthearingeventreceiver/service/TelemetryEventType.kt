8a8c46a28d2033d816bb761210d3bfe5734cdfad
Chris Faulkner <58180863+chris-faulkner-mojdt@users.noreply.github.com>
2021-09-01 16:09:48 +0100

    ✨PIC-1649 empty delete endpoint (#25)
    * ✨PIC-1649 empty delete endpoint
    
    * ✨PIC-1649 empty delete endpoint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/courthearingeventreceiver/service/TelemetryEventType.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courthearingeventreceiver/service/TelemetryEventType.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.courthearingeventreceiver.service
 
 enum class TelemetryEventType(val eventName: String) {
-  COURT_HEARING_EVENT_RECEIVED("PiCCourtHearingEventReceived")
+  COURT_HEARING_EVENT_RECEIVED("PiCCourtHearingEventReceived"),
+  COURT_HEARING_DELETE_EVENT_RECEIVED("PiCCourtHearingDeleteEventReceived")
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/courthearingeventreceiver/controller/EventControllerIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/courthearingeventreceiver/controller/EventControllerIntTest.kt
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.nhaarman.mockitokotlin2.verify
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.mock.mockito.MockBean
@@ -38,47 +39,97 @@ class EventControllerIntTest : IntegrationTestBase() {
     hearingEvent = mapper.readValue(str, HearingEvent::class.java)
   }
 
-  @Test
-  fun whenPostToEventEndpointWithRequiredRole_thenReturn204NoContent_andPushToTopic() {
+  @Nested
+  inner class ConfirmedUpdatedEndpoint {
+    @Test
+    fun whenPostToEventEndpointWithRequiredRole_thenReturn204NoContent_andPushToTopic() {
 
-    postEvent(
-      hearingEvent,
-      jwtHelper.createJwt("common-platform-events", roles = listOf("ROLE_COURT_HEARING_EVENT_WRITE"))
-    )
-      .exchange()
-      .expectStatus().isAccepted
+      postEvent(
+        hearingEvent,
+        jwtHelper.createJwt("common-platform-events", roles = listOf("ROLE_COURT_HEARING_EVENT_WRITE"))
+      )
+        .exchange()
+        .expectStatus().isOk
 
-    // Verify new thing received at topic
-    val messages = sqs.receiveMessageAsync("http://localhost:4566/000000000000/test-queue")
-      .get(5, TimeUnit.SECONDS)
-    assertThat(messages.messages.size).isEqualTo(1)
-    assertThat(messages.messages[0].body).contains("59cb14a6-e8de-4615-9c9d-94fa5ef81ad2")
+      // Verify new thing received at topic
+      val messages = sqs.receiveMessageAsync("http://localhost:4566/000000000000/test-queue")
+        .get(5, TimeUnit.SECONDS)
+      assertThat(messages.messages.size).isEqualTo(1)
+      assertThat(messages.messages[0].body).contains("59cb14a6-e8de-4615-9c9d-94fa5ef81ad2")
 
-    val expectedMap = mapOf("id" to "59cb14a6-e8de-4615-9c9d-94fa5ef81ad2", "courtCode" to "B10JQ00")
-    verify(telemetryService).trackEvent(TelemetryEventType.COURT_HEARING_EVENT_RECEIVED, expectedMap)
+      val expectedMap = mapOf("id" to "59cb14a6-e8de-4615-9c9d-94fa5ef81ad2", "courtCode" to "B10JQ00")
+      verify(telemetryService).trackEvent(TelemetryEventType.COURT_HEARING_EVENT_RECEIVED, expectedMap)
+    }
+
+    @Test
+    fun whenPostToEventEndpointWithoutRequiredRole_thenReturn403Forbidden() {
+
+      postEvent(hearingEvent, jwtHelper.createJwt("common-platform-events"))
+        .exchange()
+        .expectStatus().isForbidden
+    }
+
+    @Test
+    fun whenPostToEventEndpointWithBadToken_thenReturn401Unauthorized() {
+      val token = "bad_token"
+      postEvent(hearingEvent, token)
+        .exchange()
+        .expectStatus().isUnauthorized
+    }
+
+    private fun postEvent(hearingEvent: HearingEvent, token: String) =
+      webTestClient
+        .post()
+        .uri(String.format(UPDATED_PATH, hearingEvent.hearing.id))
+        .contentType(MediaType.APPLICATION_JSON)
+        .header("Authorization", "Bearer $token")
+        .body(Mono.just(hearingEvent), HearingEvent::class.java)
   }
 
-  @Test
-  fun whenPostToEventEndpointWithoutRequiredRole_thenReturn403Forbidden() {
+  @Nested
+  inner class DeleteEndpoint {
+    @Test
+    fun whenPostToHearingDeleteEndpointWithRequiredRole_thenReturn204NoContent_andPushToTopic() {
 
-    postEvent(hearingEvent, jwtHelper.createJwt("common-platform-events"))
-      .exchange()
-      .expectStatus().isForbidden
+      deleteEvent(
+        hearingEvent,
+        jwtHelper.createJwt("common-platform-events", roles = listOf("ROLE_COURT_HEARING_EVENT_WRITE")),
+        DELETE_PATH
+      )
+        .exchange()
+        .expectStatus().isOk
+
+      // TODO - when we understand the nature of the DELETE message to be posted, then verify the SNS posting
+
+      val expectedMap = mapOf("id" to "59cb14a6-e8de-4615-9c9d-94fa5ef81ad2")
+      verify(telemetryService).trackEvent(TelemetryEventType.COURT_HEARING_DELETE_EVENT_RECEIVED, expectedMap)
+    }
+
+    @Test
+    fun whenPostToHearingDeleteEndpointWithoutRequiredRole_thenReturn403Forbidden() {
+
+      deleteEvent(hearingEvent, jwtHelper.createJwt("common-platform-events"), DELETE_PATH)
+        .exchange()
+        .expectStatus().isForbidden
+    }
+
+    @Test
+    fun whenPostToEventEndpointWithBadToken_thenReturn401Unauthorized() {
+      val token = "bad_token"
+      deleteEvent(hearingEvent, token, DELETE_PATH)
+        .exchange()
+        .expectStatus().isUnauthorized
+    }
+
+    private fun deleteEvent(hearingEvent: HearingEvent, token: String, id: String) =
+      webTestClient
+        .delete()
+        .uri(String.format(DELETE_PATH, hearingEvent.hearing.id))
+        .header("Authorization", "Bearer $token")
   }
 
-  @Test
-  fun whenPostToEventEndpointWithBadToken_thenReturn401Unauthorized() {
-    val token = "bad_token"
-    postEvent(hearingEvent, token)
-      .exchange()
-      .expectStatus().isUnauthorized
+  companion object {
+    const val UPDATED_PATH: String = "/hearing/%s"
+    const val DELETE_PATH: String = "/hearing/%s/delete"
   }
-
-  private fun postEvent(hearingEvent: HearingEvent, token: String) =
-    webTestClient
-      .post()
-      .uri("/hearing/" + hearingEvent.hearing.id)
-      .contentType(MediaType.APPLICATION_JSON)
-      .header("Authorization", "Bearer $token")
-      .body(Mono.just(hearingEvent), HearingEvent::class.java)
 }
