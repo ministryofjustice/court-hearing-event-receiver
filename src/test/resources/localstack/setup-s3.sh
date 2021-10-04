6225aff53d16b3579d1c0371552d95e54533fead
chris-moj <christopher.faulkner@digital.justice.gov.uk>
2021-10-04 10:01:08 +0100

    ✨ PIC-1558 add S3 and upload incoming request bodies there

--- src/test/resources/localstack/setup-s3.sh
+++ src/test/resources/localstack/setup-s3.sh
@@ -7,6 +7,6 @@ export AWS_DEFAULT_REGION=eu-west-2
 export PAGER=
 
 # Create the bucket
-aws s3 --endpoint-url=http://localhost:4566 --region eu-west-2 ls s3://38c995c1-971b-4c3a-ba78-c9db73a796d0 || aws --endpoint-url=http://localhost:4566 --region=eu-west-2 s3 mb s3://38c995c1-971b-4c3a-ba78-c9db73a796d0
+aws s3 --endpoint-url=http://localhost:4566 --region eu-west-2 ls s3://local-644707540a8083b7b15a77f51641f632 || aws --endpoint-url=http://localhost:4566 --region=eu-west-2 s3 mb s3://local-644707540a8083b7b15a77f51641f632
 
 echo "S3 Configured with cpg-bucket"
