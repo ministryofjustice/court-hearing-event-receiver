8ee9dededbccbad3352f09bffe4d132f16360fde
rschallapalli <ravishankar.challapalli@digital.justice.gov.uk>
2022-03-18 17:45:13 +0000

    PIC-2029 capture and pass over contact details to court case matcher

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courthearingeventreceiver/model/Contact.kt
@@ -0,0 +1,17 @@
+package uk.gov.justice.digital.hmpps.courthearingeventreceiver.model
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class Contact(
+
+  @JsonProperty("home")
+  val home: String,
+
+  @JsonProperty("mobile")
+  val mobile: String,
+
+  @JsonProperty("work")
+  val work: String
+)
