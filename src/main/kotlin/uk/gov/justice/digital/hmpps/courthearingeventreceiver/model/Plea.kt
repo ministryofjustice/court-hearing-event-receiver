4c7da0342800ded9ca8bbf090fcdd8eaad1e8e23
vb-moj <venkatesh.balasubramanian@digital.justice.gov.uk>
2023-05-12 16:13:05 +0100

    PIC-2661
    . Added missing plea date.
    . Updated tests.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/courthearingeventreceiver/model/Plea.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courthearingeventreceiver/model/Plea.kt
@@ -1,11 +1,13 @@
 package uk.gov.justice.digital.hmpps.courthearingeventreceiver.model
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import java.time.LocalDate
 import javax.validation.constraints.NotBlank
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class Plea(
 
   @field:NotBlank
-  val pleaValue: String
+  val pleaValue: String,
+  val pleaDate: LocalDate?
 )
