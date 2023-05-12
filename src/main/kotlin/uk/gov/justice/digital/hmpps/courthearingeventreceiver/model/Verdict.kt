23899b5a674429deaea0af2682b482fd7d0e1b48
vb-moj <venkatesh.balasubramanian@digital.justice.gov.uk>
2023-05-12 16:13:05 +0100

    PIC-2661
    . Removed the validation on the case marker field, so it can be empty.
    . Updated test.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/courthearingeventreceiver/model/Verdict.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courthearingeventreceiver/model/Verdict.kt
@@ -1,9 +1,11 @@
 package uk.gov.justice.digital.hmpps.courthearingeventreceiver.model
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import java.time.LocalDate
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class Verdict(
 
-  val verdictType: VerdictType?
+  val verdictType: VerdictType?,
+  val verdictDate: LocalDate?
 )
