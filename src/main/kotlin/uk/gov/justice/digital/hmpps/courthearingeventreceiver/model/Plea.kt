664fc33f7100d91e027439a34ee4918340ed49b5
vb-moj <venkatesh.balasubramanian@digital.justice.gov.uk>
2023-05-15 06:48:52 +0100

    PIC-2661
    . Removed empty check on plea value.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/courthearingeventreceiver/model/Plea.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courthearingeventreceiver/model/Plea.kt
@@ -2,12 +2,10 @@ package uk.gov.justice.digital.hmpps.courthearingeventreceiver.model
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import java.time.LocalDate
-import javax.validation.constraints.NotBlank
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class Plea(
 
-  @field:NotBlank
   val pleaValue: String,
   val pleaDate: LocalDate?
 )
