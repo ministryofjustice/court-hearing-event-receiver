13ed8f21162d2265264e9007550e3ce6c245769c
cairnsp <philip.feroldi-cairns@digital.justice.gov.uk>
2023-09-05 10:35:18 +0100

    PIC-3285 AWS IRSA Creds refactor

--- src/main/kotlin/uk/gov/justice/digital/hmpps/courthearingeventreceiver/config/AwsConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courthearingeventreceiver/config/AwsConfig.kt
@@ -1,10 +1,9 @@
 package uk.gov.justice.digital.hmpps.courthearingeventreceiver.config
 
-import com.amazonaws.auth.AWSCredentials
-import com.amazonaws.auth.AWSStaticCredentialsProvider
-import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import com.amazonaws.services.sns.AmazonSNS
+import com.amazonaws.services.sns.AmazonSNSClientBuilder
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -15,16 +14,19 @@ import org.springframework.context.annotation.Profile
 class AwsConfig {
 
   @Bean
-  fun amazonS3Client(
-    @Value("\${aws.region-name}") regionName: String,
-    @Value("\${aws.s3.access_key_id}") s3AccessKeyId: String,
-    @Value("\${aws.s3.secret_access_key}") s3SecretAccessKey: String
-  ): AmazonS3 {
-    val credentials: AWSCredentials = BasicAWSCredentials(s3AccessKeyId, s3SecretAccessKey)
+  fun amazonS3Client(@Value("\${aws.region-name}") regionName: String): AmazonS3 {
 
     return AmazonS3ClientBuilder
       .standard()
-      .withCredentials(AWSStaticCredentialsProvider(credentials))
+      .withRegion(regionName)
+      .build()
+  }
+
+  @Bean
+  fun amazonSNSClient(@Value("\${aws.region-name}") regionName: String): AmazonSNS {
+
+    return AmazonSNSClientBuilder
+      .standard()
       .withRegion(regionName)
       .build()
   }
