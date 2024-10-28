a9c37a5f6223f0fc94fc5c937dc332901cc10140
Bingjie Liu <Bingjie.Liu@justice.gov.uk>
2024-10-28 14:36:33 +0000

    Add additional CP payload filed to domain (#467)
    * Add address, name and lja to CourtCentre
    Add orderedDate and judicialResultPrompts to JudicialResults
    Add JudicialResultsPrompt data class
    Add LJA data class
    
    * Make lja nullable and lint
    
    * Add test cases for lja and judicialResultPrompts
    
    ---------
    
    Co-authored-by: Bingjie Liu <bingjie.liu@digital.justice.gov.uk>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/courthearingeventreceiver/model/JudicialResults.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courthearingeventreceiver/model/JudicialResults.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.courthearingeventreceiver.model
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.validation.constraints.NotBlank
+import java.time.LocalDate
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class JudicialResults(
@@ -17,4 +18,10 @@ data class JudicialResults(
 
   @JsonProperty("resultText")
   val resultText: String?,
+
+  @JsonProperty("orderedDate")
+  val orderedDate: LocalDate?,
+
+  @JsonProperty("judicialResultPrompts")
+  val judicialResultPrompts: List<JudicialResultsPrompt> = emptyList(),
 )
