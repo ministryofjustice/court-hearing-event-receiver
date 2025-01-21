59d4d711a5a0265d4208740277bf7375eee04d8b
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2025-01-21 11:20:52 +0000

    Migrate to S3AsyncClient
    Migrate from S3Client to S3AsyncClient. Asynchronous calls are preferred but
    also this is used with the SNS extended client.
    
    Update tests and configurations

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courthearingeventreceiver/config/LocalAwsConfig.kt
@@ -0,0 +1,28 @@
+package uk.gov.justice.digital.hmpps.courthearingeventreceiver.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
+import software.amazon.awssdk.services.s3.S3AsyncClient
+import java.net.URI
+
+@Profile("local")
+@Configuration
+class LocalAwsConfig(
+  @Value("\${aws.region-name}")
+  var regionName: String,
+) {
+  @Value("\${aws.localstack-endpoint-url}")
+  lateinit var endpointUrl: String
+
+  @Bean
+  fun awsS3LocalStackAsyncClient(): S3AsyncClient {
+    return S3AsyncClient.builder().endpointOverride(URI(endpointUrl))
+      .endpointOverride(URI.create(endpointUrl))
+      .forcePathStyle(true).credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create("any", "any")))
+      .region(software.amazon.awssdk.regions.Region.of(regionName)).build()
+  }
+}
--- src/main/kotlin/uk/gov/justice/digital/hmpps/courthearingeventreceiver/service/S3Service.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courthearingeventreceiver/service/S3Service.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.courthearingeventreceiver.service
 
-import com.amazonaws.services.s3.AmazonS3
 import com.fasterxml.jackson.core.JsonProcessingException
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
@@ -8,6 +7,9 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
+import software.amazon.awssdk.core.async.AsyncRequestBody
+import software.amazon.awssdk.services.s3.S3AsyncClient
+import software.amazon.awssdk.services.s3.model.PutObjectRequest
 import uk.gov.justice.digital.hmpps.courthearingeventreceiver.extensions.buildS3Key
 import uk.gov.justice.digital.hmpps.courthearingeventreceiver.extensions.findUuid
 import uk.gov.justice.digital.hmpps.courthearingeventreceiver.model.HearingEvent
@@ -17,7 +19,7 @@ import java.time.LocalDateTime
 @Component
 class S3Service(
   @Value("\${aws.s3.bucket_name}") private val bucketName: String,
-  @Autowired private val amazonS3Client: AmazonS3,
+  @Autowired private val amazonS3AsyncClient: S3AsyncClient,
   @Autowired private val mapper: ObjectMapper,
 ) {
   fun uploadMessage(uriPath: String, messageContent: String): String? {
@@ -29,9 +31,9 @@ class S3Service(
     )
 
     return try {
-      val putResult = amazonS3Client.putObject(bucketName, s3Key, messageContent)
-      log.info("File {} saved to S3 bucket {} with expiration date of {}, eTag {}", s3Key, bucketName, putResult.expirationTime, putResult.eTag)
-      putResult.eTag
+      val putResult = amazonS3AsyncClient.putObject(PutObjectRequest.builder().bucket(bucketName).key(s3Key).build(), AsyncRequestBody.fromString(messageContent)).get();
+      log.info("File {} saved to S3 bucket {} with expiration date of {}, eTag {}", s3Key, bucketName, putResult.expiration(), putResult.eTag())
+      putResult.eTag()
     } catch (ex: RuntimeException) {
       // Happy to swallow this one with a log statement because failure to back up the file is not business critical
       log.error("Failed to back up file {} to S3 bucket {}", s3Key, bucketName, ex)
