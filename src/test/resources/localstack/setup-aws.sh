73990330d3a34c0d9d86f30857d3427f33464e00
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2025-01-23 12:10:55 +0000

    Set a more readable local s3 bucket name

--- src/test/resources/localstack/setup-aws.sh
+++ src/test/resources/localstack/setup-aws.sh
@@ -7,9 +7,9 @@ export AWS_DEFAULT_REGION=eu-west-2
 export PAGER=
 
 # Create the bucket
-aws s3 --endpoint-url=http://localhost:4566 --region eu-west-2 ls s3://local-644707540a8083b7b15a77f51641f632 || aws --endpoint-url=http://localhost:4566 --region=eu-west-2 s3 mb s3://local-644707540a8083b7b15a77f51641f632
+aws s3 --endpoint-url=http://localhost:4566 --region eu-west-2 ls s3://cpg-s3-bucket  || aws --endpoint-url=http://localhost:4566 --region eu-west-2 s3 mb s3://cpg-s3-bucket
 
 aws --endpoint-url=http://localhost:4566 sns create-topic --name court-cases-topic.fifo --attributes '{"FifoTopic":"true", "ContentBasedDeduplication":"true"}'
 aws --endpoint-url=http://localhost:4566 sns subscribe --topic-arn "arn:aws:sns:eu-west-2:000000000000:court-cases-topic.fifo" --protocol "sqs" --notification-endpoint "arn:aws:sns:eu-west-2:000000000000:court_cases_queue.fifo"
 
-echo "S3 Configured with cpg-bucket"
+echo "S3 Configured with cpg-s3-bucket"
