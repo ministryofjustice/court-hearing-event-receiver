ac7d1cd21dbf54b83c0c21722a5267cea06c2a20
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2025-02-04 12:05:08 +0000

    PIC-4424 remove sns subscriptions in setup script
    Remove sns topic creation and subscriptions from the script
    because this is done by the hmpps sqs library automatically

--- src/test/resources/localstack/setup-aws.sh
+++ src/test/resources/localstack/setup-aws.sh
@@ -10,7 +10,4 @@ export PAGER=
 aws s3 --endpoint-url=http://localhost:4566 --region eu-west-2 ls s3://cpg-s3-bucket  || aws --endpoint-url=http://localhost:4566 --region eu-west-2 s3 mb s3://cpg-s3-bucket
 aws s3 --endpoint-url=http://localhost:4566 --region eu-west-2 ls s3://s3-large-cases-bucket  || aws --endpoint-url=http://localhost:4566 --region eu-west-2 s3 mb s3://s3-large-cases-bucket
 
-aws --endpoint-url=http://localhost:4566 sns create-topic --name court-cases-topic.fifo --attributes '{"FifoTopic":"true", "ContentBasedDeduplication":"true"}'
-aws --endpoint-url=http://localhost:4566 sns subscribe --topic-arn "arn:aws:sns:eu-west-2:000000000000:court-cases-topic.fifo" --protocol "sqs" --notification-endpoint "arn:aws:sns:eu-west-2:000000000000:court_cases_queue.fifo"
-
-echo "S3 Configured with cpg-s3-bucket"
+echo "S3 Configured with cpg-s3-bucket and s3-large-cases-bucket"
