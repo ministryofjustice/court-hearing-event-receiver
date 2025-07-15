dd534fd00e19048e8bfe024387c077751f479d18
Mark Rees <mark.rees@digital.justice.gov.uk>
2025-07-15 10:02:09 +0100

    PI-3276 indicate if PNC CRO and isYouth are missing

--- src/main/kotlin/uk/gov/justice/digital/hmpps/courthearingeventreceiver/model/Defendant.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courthearingeventreceiver/model/Defendant.kt
@@ -2,10 +2,17 @@ package uk.gov.justice.digital.hmpps.courthearingeventreceiver.model
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer
+import com.fasterxml.jackson.databind.node.ArrayNode
 import jakarta.validation.Valid
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotEmpty
 
+@JsonDeserialize(using = DefendantDeserializer::class)
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class Defendant(
 
@@ -42,5 +49,53 @@ data class Defendant(
   val croNumber: String?,
 
   @JsonProperty("isYouth")
-  val isYouth: Boolean,
+  val isYouth: Boolean?,
+
+  val isPncMissing: Boolean,
+  val isCroMissing: Boolean,
+  val isYouthMissing: Boolean,
 )
+
+class DefendantDeserializer : StdDeserializer<Defendant>(Defendant::class.java) {
+  override fun deserialize(parser: JsonParser, context: DeserializationContext): Defendant? {
+    val node = parser.codec.readTree<JsonNode>(parser)
+    val id = node.get("id").asText()
+    val offences: List<Offence> =
+      if (!node.has("offences") || node.get("offences").isNull) {
+        emptyList()
+      } else {
+        (node.get("offences") as ArrayNode)
+          .map { parser.codec.treeToValue(it, Offence::class.java) }
+      }
+
+    val prosecutionCaseId = node.get("prosecutionCaseId").asText()
+
+    val personDefendant =
+      node.optional("personDefendant").map { parser.codec.treeToValue(it, PersonDefendant::class.java) }.orElse(null)
+    val legalEntityDefendant =
+      node.optional("legalEntityDefendant").map { parser.codec.treeToValue(it, LegalEntityDefendant::class.java) }
+        .orElse(null)
+    val masterDefendantId = node.optional("masterDefendantId").map { it.asText() }.orElse(null)
+    val isYouthMissing = !node.has("isYouth")
+    val isYouth = node.optional("isYouth").map { it.asBoolean() }.orElse(null)
+    val isPncMissing = !node.has("pncId")
+    val pncId = node.optional("pncId").map { it.asText() }.orElse(null)
+    val isCroMissing = !node.has("croNumber")
+    val croNumber = node.optional("croNumber").map { it.asText() }.orElse(null)
+
+    return Defendant(
+      id,
+      offences,
+      prosecutionCaseId,
+      personDefendant,
+      legalEntityDefendant,
+      masterDefendantId,
+      pncId,
+      croNumber,
+      isYouth,
+      isPncMissing,
+      isCroMissing,
+      isYouthMissing,
+    )
+  }
+}
