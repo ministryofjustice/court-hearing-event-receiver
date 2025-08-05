ff49a5a2e8ff911530f2a0e928dde56e4af71ef4
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2025-08-05 14:42:04 +0100

    PI-3292 court hearing delete event

--- src/main/kotlin/uk/gov/justice/digital/hmpps/courthearingeventreceiver/model/type/HearingEventType.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/courthearingeventreceiver/model/type/HearingEventType.kt
@@ -5,10 +5,12 @@ import uk.gov.justice.digital.hmpps.courthearingeventreceiver.service.TelemetryE
 enum class HearingEventType(val description: String) {
   CONFIRMED_OR_UPDATED("ConfirmedOrUpdated"),
   RESULTED("Resulted"),
+  DELETED("Deleted"),
   ;
 
   fun getTelemetryEventType(): TelemetryEventType = when (this) {
     CONFIRMED_OR_UPDATED -> TelemetryEventType.COURT_HEARING_UPDATE_EVENT_RECEIVED
     RESULTED -> TelemetryEventType.COURT_HEARING_RESULT_EVENT_RECEIVED
+    DELETED -> TelemetryEventType.COURT_HEARING_DELETE_EVENT_RECEIVED
   }
 }
