33aa3f0ea9312dada8034fb9f104a3ff5094a170
markreesmoj <76954782+markreesmoj@users.noreply.github.com>
2025-08-29 10:11:29 +0100

    CPR-857 integration test setup (#534)
    * CPR-857 get integration tests ready for gradle v9
    
    * CPR-857 run tests before integrationTest so pipeline can just run integrationTest
    
    * CPR-857 run integration tests in pipeline

--- src/test/kotlin/uk/gov/justice/digital/hmpps/courthearingeventreceiver/controller/EventControllerIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/courthearingeventreceiver/controller/EventControllerIntTest.kt
@@ -36,9 +36,6 @@ class EventControllerIntTest : IntegrationTestBase() {
   @Autowired
   lateinit var objectMapper: ObjectMapper
 
-  @Autowired
-  lateinit var amazonS3: S3AsyncClient
-
   @MockitoBean
   lateinit var telemetryService: TelemetryService
 
@@ -284,7 +281,7 @@ class EventControllerIntTest : IntegrationTestBase() {
       val messages = courtCasesQueue?.sqsClient?.receiveMessage(ReceiveMessageRequest.builder().queueUrl(courtCasesQueue?.queueUrl!!).build())!!.get()
       assertThat(messages.messages().size).isEqualTo(1)
       val message: SQSMessage = objectMapper.readValue(messages.messages()[0].body(), SQSMessage::class.java)
-      assertThat(message.message).contains("\"hearingId\": \"59cb14a6-e8de-4615-9c9d-94fa5ef81ad2\"")
+      assertThat(message.message).contains("\"hearingId\":\"59cb14a6-e8de-4615-9c9d-94fa5ef81ad2\"")
 
       val expectedMap = mapOf("id" to "59cb14a6-e8de-4615-9c9d-94fa5ef81ad2")
       verify(telemetryService).trackEvent(TelemetryEventType.COURT_HEARING_DELETE_EVENT_RECEIVED, expectedMap)
