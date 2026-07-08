d5766ec800719b967c0a911a15ffaa3aca50ae2b
Sam Bailey <161320275+sambailey-moj@users.noreply.github.com>
2026-07-08 10:59:32 +0100

    bump dependencies (#617)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.5.3"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.5.6"
   kotlin("plugin.spring") version "2.4.0"
 }
 
